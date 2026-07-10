b287a672bdda5d690adb71d7168872c5920df188
Siddharth Rajit <siddharth.rajit2207@gmail.com>
2026-07-10 13:32:34 -0300

    Clean up files

---
+++ Practice-Projects/TaskCLI/src/Todo.java
@@ -0,0 +1,86 @@
+public class Todo {
+
+    private int id;
+    private static int lastId = 0;
+    private String description;
+    private Status status;
+
+    public Todo(String description) {
+        this.id = ++lastId;
+        this.description = description;
+        this.status = Status.TODO;
+    }
+
+    public Todo(int id, String description, Status status) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+
+        if (id > lastId) {
+            lastId = id;
+        }
+    }
+
+    public String toJson() {
+        return "{"
+                + "\"id\":" + id + ","
+                + "\"status\":\"" + status + "\","
+                + "\"description\":\"" + description + "\""
+                + "}";
+    }
+
+    public static Todo fromJson(String json) {
+        json = json.replace("{", "")
+                   .replace("}", "")
+                   .replace("\"", "");
+
+        String[] parts = json.split(",");
+
+        int id = 0;
+        String description = "";
+        Status status = Status.TODO;
+
+        for (String part : parts) {
+            String[] keyValue = part.split(":");
+
+
+            String key = keyValue[0].trim();
+            String value = keyValue[1].trim();
+
+
+            switch (key) {
+
+                case "id":
+                    id = Integer.parseInt(value);
+                    break;
+
+                case "status":
+                    status = Status.valueOf(value);
+                    break;
+
+                case "description":
+                    description = value;
+                    break;
+            }
+        }
+        return new Todo(id, description, status);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + ", description: " + description.strip() + ", status: " + status.toString();
+    }
+
+}
---
+++ Practice-Projects/TaskCLI/src/TodoService.java
@@ -0,0 +1,91 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoService {
+
+    private final Path FILE_PATH = Path.of("todos.json");
+    private List<Todo> todos;
+
+    public TodoService() {
+        todos = new ArrayList<>();
+
+        try{
+            loadTodos();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addTodo(String description) {
+        Todo newTodo = new Todo(description);
+        todos.add(newTodo);
+        saveChanges();
+        System.out.println("Task added successfully (ID: " + newTodo.getId() + ")");
+    }
+
+    public void listTodos(String type) {
+        for(Todo todo : todos) {
+            String status = todo.getStatus().toString().strip();
+
+            if (type.equals("All") || status.equals(type)){
+                System.out.println(todo.toString());
+            }
+        }
+    }
+
+    public void saveTodos() throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[\n");
+
+        for (int i = 0; i < todos.size(); i++) {
+            sb.append(todos.get(i).toJson());
+
+            if (i != todos.size() - 1) {
+                sb.append(",");
+            }
+
+            sb.append("\n");
+        }
+
+        sb.append("]");
+
+        Files.writeString(FILE_PATH, sb.toString());
+    }
+
+    public void loadTodos() throws IOException {
+        if (!Files.exists(FILE_PATH)) {
+            return;
+        }
+
+        String content = Files.readString(FILE_PATH);
+
+        if (content.equals("[]")) {
+            return;
+        }
+
+        content = content.substring(1, content.length() - 1);
+
+        String[] objects = content.split("},");
+
+        for (String object : objects) {
+            if (!object.endsWith("}")) {
+                object += "}";
+            }
+            todos.add(Todo.fromJson(object));
+        }
+
+
+    }
+
+    private void saveChanges() {
+        try {
+            saveTodos();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
