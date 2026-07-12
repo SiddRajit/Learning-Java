9eed6c9d20f327602e7f0d6b3082923d8178b232
Siddharth Rajit <siddharth.rajit2207@gmail.com>
2026-07-12 18:30:39 -0300

    Add initial project structure for Library Management CLI with .gitignore and pom.xml

---
+++ Practice-Projects/LibraryMangemaneCLI/src/main/java/LibraryManagementCLI.java
@@ -0,0 +1,13 @@
+
+
+public class LibraryManagementCLI {
+
+  public static void main(String[] args) {
+    if (args.length < 0) {
+      System.out.println("You need to pass a command!");
+      System.out.println("Usage: ");
+    }
+
+    System.out.println("Welcome to Library Management CLI");
+  }
+}
